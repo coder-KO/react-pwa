7430b5c6bf19c679e57dd867ed3ea5be02d3f4af
Krishna Ojha <erkaojha@gmail.com>
2023-03-16 00:40:58 +0530

    ADD: initial file structure and code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#2B2B2B",
+        color: "#05EDFE",
+      }}
+    >
+      <img src="/icons/logoBg.png" alt="React PWA" width="240" height="auto" />
+      <h1>React PWA</h1>
+    </div>
+  );
+};
+
+export default App;
